55683ec6314bee2ce7ad55d0c61a363a73014908
zahra <zahraabdallajin@gmail.com>
2024-03-21 11:56:26 +0300

    creating a dashboard

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 console.log("history")
-const herocontent=document.querySelector(".hero__content");
+const heroform=document.querySelector(".hero__form");
 const loginlink=document.querySelector(".login__link");
 const registerlink=document.querySelector(".register__link");
 const btnlogin=document.querySelector(".btn__login");
-
+const iconclose=document.querySelector(".icon__close");
 
 registerlink.addEventListener('click',()=>{
-    herocontent.classList.add('active');
+    heroform.classList.add('active');
 })
 
 loginlink.addEventListener('click',()=>{
-    herocontent.classList.remove('active');
+    heroform.classList.remove('active');
 })
 
 btnlogin.addEventListener('click',()=>{
-    herocontent.classList.add('active-login');
+    heroform.classList.add('active-login');
+})
+
+iconclose.addEventListener('click',()=>{
+    heroform.classList.remove('active-login');
 })
 
