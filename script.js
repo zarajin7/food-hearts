c30feaf713c16cadddcadd4f114e267b2b339b9a
zahra <zahraabdallajin@gmail.com>
2024-03-21 13:43:09 +0300

    made a js file

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
 console.log("history")
-const heroform=document.querySelector(".hero__form");
-const loginlink=document.querySelector(".login__link");
-const registerlink=document.querySelector(".register__link");
-const btnlogin=document.querySelector(".btn__login");
-const iconclose=document.querySelector(".icon__close");
-
-registerlink.addEventListener('click',()=>{
-    heroform.classList.add('active');
-})
-
-loginlink.addEventListener('click',()=>{
-    heroform.classList.remove('active');
-})
-
-btnlogin.addEventListener('click',()=>{
-    heroform.classList.add('active-login');
-})
-
-iconclose.addEventListener('click',()=>{
-    heroform.classList.remove('active-login');
-})
+const allSideMenu=document.querySelectorAll('')
 
